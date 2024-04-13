c5df331296ba2a5f6d89aa6fd8634356edaaf114
Arjun Umathanu <47590064+fullstack-spiderman@users.noreply.github.com>
2024-04-14 01:44:46 +0530

    Fix workflows ubuntu python version (#4)
    * Downgrade python version in gh-actions as it does not found in the Ubuntu 22.04.
    
    * Increment app's patch version.
    
    * Add custom pytest fixture to yield httpx_mock.
    
    * Will add test_cli with mocks and stubs, later.
    
    * Add test cases for test_crud_manager_get, test_crud_manager_post and test_crud_manager_put. Will add remaining soon.
    
    * Add test case for test_crud_manager_patch.
    
    * Update Endpoints_Under_Tests (EUTs).
    
    * Add test case for test_crud_manager_delete.
    
    * Increment app's patch version.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,17 +1,11 @@
+import json
 from datetime import timedelta
-from typing import Any, Generator
+from typing import Any
 
-import pytest
 from pytest_httpx import HTTPXMock
 
-import restx
-import restx.enums
+from restx.enums import HTTPMethod
 from restx.utils import crud_manager
-
-
-@pytest.fixture
-def mock_client(httpx_mock: HTTPXMock) -> Generator[HTTPXMock, None, None]:
-    yield httpx_mock
 
 
 def test_crud_manager_get(httpx_mock: HTTPXMock) -> None:
@@ -23,10 +17,100 @@ def test_crud_manager_get(httpx_mock: HTTPXMock) -> None:
 
     # Act
     result: dict[str, Any] = crud_manager(
-        url=url, method=restx.enums.HTTPMethod.GET.value, headers=headers
+        url=url, method=HTTPMethod.GET.value, headers=json.dumps(headers)
     )
 
     # Assert
     assert result["response"].status_code == 200
+    assert isinstance(result["response_time"], timedelta)
+    assert result["response"].json() == response_data
+
+
+def test_crud_manager_post(httpx_mock: HTTPXMock) -> None:
+    # Arrange
+    url = "https://example.com"
+    payload: dict[str, str] = {"key": "value"}
+    headers: dict[str, str] = {"Authorization": "Bearer token"}
+    response_data: dict[str, str] = {"status": "success"}
+    httpx_mock.add_response(method="POST", status_code=201, url=url, json=response_data)
+
+    # Act
+    result: dict[str, Any] = crud_manager(
+        url=url,
+        method=HTTPMethod.POST.value,
+        payload=json.dumps(payload),
+        headers=json.dumps(headers),
+    )
+
+    # Assert
+    assert result["response"].status_code == 201
+    assert isinstance(result["response_time"], timedelta)
+    assert result["response"].json() == response_data
+
+
+def test_crud_manager_put(httpx_mock: HTTPXMock) -> None:
+    # Arrange
+    url = "https://example.com/5"
+    payload: dict[str, str] = {"key": "value"}
+    headers: dict[str, str] = {"Authorization": "Bearer token"}
+    response_data: dict[str, str] = {"status": "success"}
+    httpx_mock.add_response(method="PUT", status_code=200, url=url, json=response_data)
+
+    # Act
+    result: dict[str, Any] = crud_manager(
+        url=url,
+        method=HTTPMethod.PUT.value,
+        payload=json.dumps(payload),
+        headers=json.dumps(headers),
+    )
+
+    # Assert
+    assert result["response"].status_code == 200
+    assert isinstance(result["response_time"], timedelta)
+    assert result["response"].json() == response_data
+
+
+def test_crud_manager_patch(httpx_mock: HTTPXMock) -> None:
+    # Arrange
+    url = "https://example.com/13"
+    payload: dict[str, str] = {"key": "value"}
+    headers: dict[str, str] = {"Authorization": "Bearer token"}
+    response_data: dict[str, str] = {"status": "success"}
+    httpx_mock.add_response(
+        method="PATCH", status_code=200, url=url, json=response_data
+    )
+
+    # Act
+    result: dict[str, Any] = crud_manager(
+        url=url,
+        method=HTTPMethod.PATCH.value,
+        payload=json.dumps(payload),
+        headers=json.dumps(headers),
+    )
+
+    # Assert
+    assert result["response"].status_code == 200
+    assert isinstance(result["response_time"], timedelta)
+    assert result["response"].json() == response_data
+
+
+def test_crud_manager_delete(httpx_mock: HTTPXMock) -> None:
+    # Arrange
+    url = "https://example.com/4"
+    headers: dict[str, str] = {"Authorization": "Bearer token"}
+    response_data: dict[str, str] = {}
+    httpx_mock.add_response(
+        method="DELETE", status_code=204, url=url, json=response_data
+    )
+
+    # Act
+    result: dict[str, Any] = crud_manager(
+        url=url,
+        method=HTTPMethod.DELETE.value,
+        headers=json.dumps(headers),
+    )
+
+    # Assert
+    assert result["response"].status_code == 204
     assert isinstance(result["response_time"], timedelta)
     assert result["response"].json() == response_data
