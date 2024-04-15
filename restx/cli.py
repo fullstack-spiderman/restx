0c4c31cf7dcafee7bff89b76b873708dbb62a257
Arjun KayalMoni <fullstack.spiderman@gmail.com>
2024-04-15 13:32:02 +0530

    Refactor patch command to optionally take args to toggle ssl_verify and follow_redirects for http.

--- restx/cli.py
+++ restx/cli.py
@@ -86,10 +86,21 @@ def patch(
     url: str,
     payload: str = typer.Option(..., "--payload", "-p", help="Payload data"),
     headers: str = typer.Option(None, "--header", "-H", help="Custom headers"),
+    disable_ssl_verify: bool = typer.Option(
+        False, "--disable-ssl-verify", help="Disable SSL certificate verification"
+    ),
+    follow_redirects: bool = typer.Option(
+        False, "--follow-redirects", help="Follow HTTP redirects"
+    ),
 ) -> None:
     """Send a PATCH request."""
     response, response_time = crud_manager(
-        url=url, method="PATCH", payload=payload, headers=headers
+        url=url,
+        method="PATCH",
+        payload=payload,
+        headers=headers,
+        disable_ssl_verify=disable_ssl_verify,
+        follow_redirects=follow_redirects,
     ).values()
     pretty_print_response(response, response_time)
 
